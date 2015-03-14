cd55cfdd17de06f0a36c05c65c318dfe2ef2f8a5
oka <ogiekako@gmail.com>
2015-03-14 18:44:58 +0900

    Update searchall.sh

--- src/cui/Main.java
+++ src/cui/Main.java
@@ -10,13 +10,29 @@ import main.PolyArray;
 
 public class Main {
 
+  /*
+  Usage:
+  java -cp bin cui.Main [--maxdepth=d] [--min_num_cand=o] [--max_num_cand=0] problemFile candFile
+  --maxdepth=d は、最大深さを指定する。覆うのに使えるセルは、バウンディングボックスから深さ d までのセルであり、
+  差し込める深さも d までとなる。
+  --min_num_cand=o
+  --max_num_cand=o はそれぞれ、覆うのに使用するポリオミノの最小、最大枚数をしていする。
+   */
   public static void main(String[] args) throws FileNotFoundException, NoCellException {
     int p = 0;
     int maxDepth = Integer.MAX_VALUE / 2;
+    int minNumCand = 1;
+    int maxNumCand = Integer.MAX_VALUE;
     while (args[p].startsWith("--")) {
       String opt = args[p++];
-      if (opt.equals("--maxdepth")) {
-        maxDepth = Integer.valueOf(args[p++]);
+      if (opt.startsWith("--maxdepth")) {
+        maxDepth = Integer.valueOf(opt.replace("--maxdepth=",""));
+      }
+      if (opt.startsWith("--min_num_cand")) {
+        minNumCand = Integer.valueOf(opt.replace("--min_num_cand=",""));
+      }
+      if (opt.startsWith("--max_num_cand")) {
+        maxNumCand = Integer.valueOf(opt.replace("--max_num_cand=",""));
       }
     }
     String problemFileName = args[p++];
@@ -26,7 +42,10 @@ public class Main {
     PolyArray problem = PolyArray.load(problemIn);
     PolyArray cand = PolyArray.load(candIn);
     boolean ok =
-        Judge.newBuilder(problem, cand).setEnabledCandDepth(maxDepth).build().judge() == null;
+        Judge.newBuilder(problem, cand)
+            .setEnabledCandDepth(maxDepth)
+            .setMinNumCands(minNumCand)
+            .setMaxNumCands(maxNumCand).build().judge() == null;
     System.out.println(ok ? "OK" : "NG");
   }
 }
--- src/searchall.sh
+++ src/searchall.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 # Run on polycover directory.
-problemDir=$1
-candDir=$2
+# For problems, only files with extension .no will be iterated.
+# Example:
+# src/searchall.sh --min_num_cand=2 --max_num_cand=3 problem/hexomino ans/hexomino 2> /dev/null
+declare min_num_cand=""
+declare max_num_cand=""
+declare problemDir=""
+declare candDir=""
+for o in $@; do
+    case $o in
+        --min_num_cand=*)
+            min_num_cand="--min_num_cand=${o#*=}"
+        ;;
+        --max_num_cand=*)
+            max_num_cand="--max_num_cand=${o#*=}"
+        ;;
+        *)
+            if [ -z $problemDir ]; then
+                problemDir=$o
+            else
+                candDir=$o
+            fi
+        ;;
+    esac
+done
 if [ -z $problemDir ] || [ -z $candDir ]; then
-	echo "Usage: src/searchall.sh problemDir candDir"
+	echo "Usage: src/searchall.sh [--min_num_cand=o] [--max_num_cand=o] problemDir candDir"
 	exit 1;
 fi
 
@@ -12,27 +34,32 @@ pushd src
 javac -d ../bin cui/Main.java
 popd
 echo "problem:"
-for a in $(find $problemDir -mindepth 1 -maxdepth 1 -type f); do
-	echo $a
-done
-echo "cand:"
-for b in $(find $candDir -mindepth 1 -maxdepth 1 -type f); do
-	echo $b
-done
-for a in $(find $problemDir -mindepth 1 -maxdepth 1 -type f); do
+for a in $(find $problemDir -mindepth 1 -type f); do
 	if [[ ${a##*.} != "no" ]]; then
 		continue
 	fi
-	for b in $(find $candDir -mindepth 1 -maxdepth 1 -type f); do
+	echo $a
+done
+echo "cand:"
+for b in $(find $candDir -mindepth 1 -type f); do
+	echo $b
+done
+for a in $(find $problemDir -mindepth 1 -type f); do
+	if [[ ${a##*.} != "no" ]]; then
+		continue
+	fi
+	for b in $(find $candDir -mindepth 1 -type f); do
 		declare maxD=$(($(head -n 1 $b | cut -d ' ' -f 1)/2+1))
 		declare ok=1
 		for d in $(seq 1 $maxD); do
-            if [[ $(java -classpath bin cui.Main --maxdepth $d $a $b) != "OK" ]]; then
+		    declare cmd="java -classpath bin cui.Main ${min_num_cand} ${max_num_cand} --maxdepth=$d $a $b"
+            echo "cmd: " $cmd >&2
+            if [[ $(${cmd}) != "OK" ]]; then
 					ok=0
 					break
 			fi
 		done
-		if [[ ok == 1 ]]; then
+		if [[ ${ok} == 1 ]]; then
 			echo  "OK: " $a $b
 		else
 			echo "NG: " $a $b $d

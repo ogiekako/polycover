040649406cb07ef9064beeffb02ce059f1310229
oka <ogiekako@gmail.com>
2015-03-21 02:33:59 +0900

    Add yes2me.sh and cleanup answers

--- javatests/longtest/JudgeTestLong.java
+++ javatests/longtest/JudgeTestLong.java
@@ -1,12 +1,17 @@
-package main;
+package longtest;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import main.Covering;
+import main.Judge;
+import main.PolyArray;
+import main.Stopwatch;
 import util.Debug;
 import util.FileUtil;
 
@@ -57,23 +62,31 @@ public class JudgeTestLong {
 
     Stopwatch latencyMetric = new Stopwatch();
     List<String> problems = FileUtil.allFilesUnder(new File("problem"));
+    List<String> answers = new ArrayList<String>();
     for (String probPath : problems) {
       if (probPath.endsWith(".no")) {
         for (String ansPath : testAnsPaths) {
-          if (!ansPath.endsWith(".ans"))continue;
-          System.err.printf("%s %s\n", probPath, ansPath);
+          if (!ansPath.endsWith(".ans")) {
+            continue;
+          }
+          String comb = String.format("%s %s", probPath, ansPath);
           Scanner probIn = new Scanner(new File(probPath));
           Scanner ansIn = new Scanner(new File(ansPath));
           PolyArray prob = PolyArray.load(probIn);
           PolyArray ans = PolyArray.load(ansIn);
           Covering result =
               Judge.newBuilder(prob, ans).setLatencyMetric(latencyMetric).build().judge().covering;
-          Assert.assertNotNull(result);
-
-          System.err.println(latencyMetric.summary());
+          if (result == null) {
+            answers.add(comb);
+          }
         }
       }
     }
+    System.err.println(latencyMetric.summary());
+    for (String s : answers) {
+      System.err.println(s);
+    }
+    Assert.assertTrue(answers.isEmpty());
   }
 
 }
---
+++ src/yes2meh.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Run in polycover directory.
+# Example: src/no2meh.sh problem/7 problem
+# rename some.no under problem/7 to some.meh if under problem there is a .yes file that is contained in some.no .
+
+# allunder dir extension -> results
+function allunder {
+    declare -r fs=$(find "$1")
+    declare -r ext="$2"
+    declare res=""
+    for f in $fs; do
+        if [ "${f}" != "${f%.$ext}" ]; then
+            declare res="${res} ${f}"
+        fi
+    done
+    echo $res
+}
+javac -d bin -sourcepath src src/util/MehDecider.java
+declare -r tgt=$1
+declare -r prob=$2
+declare -r allyes="$(allunder ${tgt} yes)"
+echo $allno
+for f in ${allyes}; do
+    declare res=$(java -cp bin util/MehDecider $f ${prob})
+    if [ ${res} == "YES" ]; then
+        mv $f "${f%.yes}.meh"
+    fi
+done
